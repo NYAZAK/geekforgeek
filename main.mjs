20398042c6c86f660fef67fee5cb429e645917e4
NYAZAK <WellP@pa6>
2021-08-21 15:31:26 +0200

    add bubble sort

--- main.mjs
+++ main.mjs
@@ -1,6 +1,9 @@
+import { BubbleSort } from './algo/bublesort.mjs';
 import { LinearSearch } from './algo/linearSearch.mjs';
 import { SelectionSort } from './algo/selectionSort.mjs';
 //const linearSearchExiste = new LinearSearch(55, [1, 2, 3, 10, 4, 0]);
 //const linearSearchNExiste = new LinearSearch(10, [1, 2, 3, 10, 4, 0]);
 
-const selectionSort =  new SelectionSort([1, 23, 43, 22, 34, 55]);
+//const selectionSort =  new SelectionSort([1, 23, 43, 22, 34, 55]);
+
+const bubbleSort =  new BubbleSort([1, 23, 43, 22, 34, 55]);
